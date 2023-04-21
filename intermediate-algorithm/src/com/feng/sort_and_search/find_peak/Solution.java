10ea1f09cc813357aca2891a49984a42b09446d2
gf9276 <927621609@qq.com>
2023-04-21 19:58:58 +0800

    2023年4月21日，中级算法

---
+++ intermediate-algorithm/src/com/feng/sort_and_search/find_peak/Solution.java
@@ -0,0 +1,24 @@
+package com.feng.sort_and_search.find_peak;
+
+/**
+ * ClassName: Solution
+ * Package: com.feng.sort_and_search.find_peak
+ * Description:
+ *
+ * @Author guof
+ * @Create 2023/4/21 19:52
+ * @Version 1.0
+ */
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int idx = 0;
+        while (true) {
+            if (idx < nums.length - 1 && nums[idx] < nums[idx + 1]) {
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return idx;
+    }
+}
