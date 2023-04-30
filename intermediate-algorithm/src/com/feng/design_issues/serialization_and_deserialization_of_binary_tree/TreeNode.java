0fc28d576c06a6923bc798a12f964746b77aa6e6
gf9276 <927621609@qq.com>
2023-04-30 22:38:38 +0800

    2023-04-30，中级算法（数学）

---
+++ intermediate-algorithm/src/com/feng/design_issues/serialization_and_deserialization_of_binary_tree/TreeNode.java
@@ -0,0 +1,20 @@
+package com.feng.design_issues.serialization_and_deserialization_of_binary_tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
