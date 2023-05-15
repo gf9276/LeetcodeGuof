8300da64166daaf9021365c9059099a6ec7dee18
gf9276 <927621609@qq.com>
2023-05-15 22:22:10 +0800

    2023-05-15，数组类算法，人生呐

---
+++ array-class-algorithm/src/com/feng/applying_basic_algorithmic_ideas/merge/Test.java
@@ -0,0 +1,19 @@
+package com.feng.applying_basic_algorithmic_ideas.merge;
+
+import java.util.Arrays;
+
+/**
+ * ClassName: Test
+ * Package: com.feng.applying_basic_algorithmic_ideas.merge
+ * Description:
+ *
+ * @Author guof
+ * @Create 2023/5/15 22:15
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solu = new Solution();
+        solu.merge(new int[]{1, 2, 4, 5, 6, 0}, 5, new int[]{3}, 1);
+    }
+}
