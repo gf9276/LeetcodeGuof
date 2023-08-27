0efa077f9bee6db67dd3cb82275dc953bc045def
gf9276 <927621609@qq.com>
2023-08-27 23:28:39 +0800

    2023/8/27

---
+++ arrays-and-string/src/feng/double_pointer_technique/max_num_of_consecutive1/Test.java
@@ -0,0 +1,18 @@
+package feng.double_pointer_technique.max_num_of_consecutive1;
+
+/**
+ * ClassName: Test
+ * Package: com.feng.applying_basic_algorithmic_ideas.merge
+ * Description:
+ *
+ * @Author guof
+ * @Create 2023/5/15 22:15
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
