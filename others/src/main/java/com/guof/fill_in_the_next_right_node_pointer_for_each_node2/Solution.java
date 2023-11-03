08681c74b9360c106898fab3424376420f858572
gf9276 <927621609@qq.com>
2023-11-03 10:18:25 +0800

    11/3，每日一题，白送

---
+++ others/src/main/java/com/guof/fill_in_the_next_right_node_pointer_for_each_node2/Solution.java
@@ -0,0 +1,35 @@
+package com.guof.fill_in_the_next_right_node_pointer_for_each_node2;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node curNode = queue.poll();
+                if (i != size - 1) {
+                    curNode.next = queue.peek();
+                } else {
+                    curNode.next = null;
+                }
+                if (curNode.left != null) {
+                    queue.add(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.add(curNode.right);
+                }
+            }
+        }
+        return root;
+    }
+}
